d7b4b6d869add88e5dfa6523fd342fd12fbf87ab
silvia.kulenovic <silviakulenovic@yahoo.com>
2019-03-14 11:21:40 +0100

    sitno dodano prognoza

--- src/components/Forecast.vue
+++ src/components/Forecast.vue
@@ -1,6 +1,6 @@
 <template>
-<div>
-  <div class="container">
+<div class="container" >
+  <div >
     <div class="box">
       <h1>{{selected}}</h1>
       <h1>4°C</h1>
@@ -13,14 +13,44 @@
         <!-- <img :src="getImgUrl(hour.weather)" v-bind:alt="hour"> -->
        <graf class="graf"></graf>
      </p>
-  <div class="container">
+  <div class="box weather">
      <div class="row">
-        <div class="col tri">
+        <div class="col tri ">
           <h3>Monday</h3>
           <p>4°C</p>
           <p><img class="slika" src="../assets/43.png"></p>
           
         </div>
+
+        <div class="col tri">
+          <h3>Monday</h3>
+          <p>4°C</p>
+          <p><img class="slika" src="../assets/35.png"></p>
+          
+        </div>
+
+        <div class="col tri">
+          <h3>Monday</h3>
+          <p>4°C</p>
+          <p><img class="slika" src="../assets/24.png"></p>
+          
+        </div>
+
+        <div class="col tri">
+          <h3>Monday</h3>
+          <p>4°C</p>
+          <p><img class="slika" src="../assets/24.png"></p>
+          
+        </div>
+
+        <div class="col tri">
+          <h3>Monday</h3>
+          <p>4°C</p>
+          <p><img class="slika" src="../assets/24.png"></p>
+          
+        </div>
+
+       
      </div>
   </div>
     </div>
@@ -108,18 +138,40 @@ export default {
 <style scoped>
 
 .container {
-}
-
-.weather {
-  width: 500px;
-  height: 600px;
-  padding: 10px 30px;
-  /*border: 1px solid black;*/
+  display: inline-flex;
   justify-content: center;
+  padding: 0 100px;
+  height: 100vh;
+
 }
+.row {
+  display: flex;
+}
+.tri{
+  padding: 20px;
+  margin-top: 20px;
+  /*width: 30%;*/
+  height: 250px;
+  
+
+}
+.weather {
+  width: 90%;
+  height: 600px;
+  padding: 10px 20px;
+  
+  justify-content: center;
+  display:  inline-flex;
+}
+.weather .tri {
+  border: 1px solid white;
+  padding: 10px;
+  margin: 10px;
+}
+ 
 
 #forecast {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: /*'Avenir', Helvetica, Arial, */sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
