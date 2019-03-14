06975a732ae2c9385a40f0fac1b66560849db1ad
LightzOut <mpaleka@gmail.com>
2019-03-14 23:39:07 +0100

    komit

---
+++ src/components/SowingCalendar.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+      <div id="app">
+        <h1>My Calendar</h1>
+        <calendar-view
+          :show-date="showDate"
+          class="theme-default holiday-us-traditional holiday-us-official">
+          <calendar-view-header
+            slot="header"
+            slot-scope="t"
+            :header-props="t.headerProps"
+            @input="setShowDate" />
+        </calendar-view>
+      </div>
+    </div>
+</template>
+<script>
+    import { CalendarView, CalendarViewHeader } from "vue-simple-calendar"
+    export default {
+        name: "SowingCalendar",
+      data: function() {
+          return {
+            showDate: new Date(),
+
+          }
+      },
+
+      components: {
+         CalendarView,
+        CalendarViewHeader,
+      },
+      methods: {
+        setShowDate(d) {
+          this.showDate = d;
+        },
+      }
+    }
+
+</script>
+
+<style scoped>
+
+</style>
