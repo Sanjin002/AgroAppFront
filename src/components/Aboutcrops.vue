e9eef2d2517df643f40fa620184022f48eb71dd7
Sanjin002 <45363041+Sanjin002@users.noreply.github.com>
2019-03-15 01:11:04 +0100

    Alive

---
+++ src/components/Aboutcrops.vue
@@ -0,0 +1,44 @@
+<template>
+	<div>
+		<div v-if="showModal">
+      <transition name="modal">
+        <div class="modal-mask">
+          <div class="modal-wrapper">
+
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title">Modal title</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true" @click="showModal = false">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <h2>DESCRIPTION</h2>
+                  <h3>{{crops.category}}</h3>
+                  <h3>{{crops.uses}}</h3>
+                  <h3>{{crops.growing_period}}</h3>
+                  <h3>{{crops.further_info}}</h3>
+                  <h2>DATA</h2>
+                  <h3>{{crops.temp_abs_min}}</h3>
+                  <h3>{{crops.temp_abs_max}}</h3>
+                  <h3>{{crops.temp_opt_min}}</h3>
+                  <h3>{{crops.temp_opt_max}}</h3>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" @click="showModal = false">Close</button>
+                  <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </transition>
+    </div>
+
+	</div>
+</template>
+
+<script>
+
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,6 @@ import Router from 'vue-router'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-
 import Hello from '@/components/Hello'
 import Crops from '@/components/Crops'
 import Forecast from '@/components/Forecast'
