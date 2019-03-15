a75e1d58acbb5a10659109f833406cd69c929d68
Sanjin002 <45363041+Sanjin002@users.noreply.github.com>
2019-03-15 01:35:26 +0100

    Calendar dobiva koji je grad

--- src/components/SowingCalendar.vue
+++ src/components/SowingCalendar.vue
@@ -2,6 +2,7 @@
     <div>
       <div id="app">
         <h1>My Calendar</h1>
+        <h1>primio sam podatak o gradu: {{selected}}</h1>
         <calendar-view
           :show-date="showDate"
           class="theme-default holiday-us-traditional holiday-us-official">
@@ -15,16 +16,21 @@
     </div>
 </template>
 <script>
-    import { CalendarView, CalendarViewHeader } from "vue-simple-calendar"
+    import { bus } from '../main';
+    import { CalendarView, CalendarViewHeader } from "vue-simple-calendar";
     export default {
         name: "SowingCalendar",
       data: function() {
           return {
             showDate: new Date(),
+            selected:'',
 
           }
       },
-
+      created(){
+    bus.$on('podaciZaGraf', (data)=> {this.selected = data;
+    })
+  },
       components: {
          CalendarView,
         CalendarViewHeader,
